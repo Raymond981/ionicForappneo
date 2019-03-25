4037dc1d1b06d0594bb70645c560af6e405c2aba
Raymond981 <163163@ids.upchiapas.edu.mx>
2019-03-25 15:30:32 -0600

    Implementación Firebase
    Firebase

--- Forappneo/src/app/app.component.ts
+++ Forappneo/src/app/app.component.ts
@@ -4,10 +4,24 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase'; 
+
+var config = {
+  apiKey: "AIzaSyDceY02_NYos2dMehvSeY-U4r4BEDJqFgU",
+  authDomain: "forappneo.firebaseapp.com",
+  databaseURL: "https://forappneo.firebaseio.com",
+  projectId: "forappneo",
+  storageBucket: "forappneo.appspot.com",
+  messagingSenderId: "127246607834"
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
+
+
+
 export class AppComponent {
   constructor(
     private platform: Platform,
@@ -22,5 +36,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
